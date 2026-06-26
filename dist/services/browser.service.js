91b376a5b9aca864245e070e8650c6596490be95
MIGHTY EAGLE <mightyeaglecorp@gmail.com>
2026-06-26 18:48:12 +0100

    made changes

--- dist/services/browser.service.js
+++ dist/services/browser.service.js
@@ -1,16 +1,30 @@
 "use strict";
+// import puppeteer, { Browser } from "puppeteer";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.closeBrowser = exports.getBrowser = void 0;
+// let browser: Browser | null = null;
+// export const getBrowser = async () => {
+//   if (browser) {
+//     return browser;
+//   }
+//   browser = await puppeteer.launch({
+//     headless: true,
+//     args: [
+//       "--no-sandbox",
+//       "--disable-setuid-sandbox",
+//       "--disable-dev-shm-usage",
+//     ],
+//   });
+//   return browser;
+// };
 const puppeteer_1 = __importDefault(require("puppeteer"));
-let browser = null;
 const getBrowser = async () => {
-    if (browser) {
-        return browser;
-    }
-    browser = await puppeteer_1.default.launch({
+    return await puppeteer_1.default.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH ||
+            "/opt/render/.cache/puppeteer/chrome/linux-150.0.7871.24/chrome-linux64/chrome",
         headless: true,
         args: [
             "--no-sandbox",
@@ -18,13 +32,12 @@ const getBrowser = async () => {
             "--disable-dev-shm-usage",
         ],
     });
-    return browser;
 };
 exports.getBrowser = getBrowser;
 const closeBrowser = async () => {
-    if (browser) {
-        await browser.close();
-        browser = null;
-    }
+    // if (browser) {
+    //   await browser.close();
+    //   browser = null;
+    // }
 };
 exports.closeBrowser = closeBrowser;
--- src/services/browser.service.ts
+++ src/services/browser.service.ts
@@ -1,27 +1,45 @@
-import puppeteer, { Browser } from "puppeteer";
+// import puppeteer, { Browser } from "puppeteer";
 
-let browser: Browser | null = null;
+// let browser: Browser | null = null;
+
+// export const getBrowser = async () => {
+//   if (browser) {
+//     return browser;
+//   }
+
+//   browser = await puppeteer.launch({
+//     headless: true,
+//     args: [
+//       "--no-sandbox",
+//       "--disable-setuid-sandbox",
+//       "--disable-dev-shm-usage",
+//     ],
+//   });
+
+//   return browser;
+// };
+
+import puppeteer from "puppeteer";
 
 export const getBrowser = async () => {
-  if (browser) {
-    return browser;
-  }
+  return await puppeteer.launch({
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH ||
+      "/opt/render/.cache/puppeteer/chrome/linux-150.0.7871.24/chrome-linux64/chrome",
 
-  browser = await puppeteer.launch({
     headless: true,
+
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
     ],
   });
-
-  return browser;
 };
 
 export const closeBrowser = async () => {
-  if (browser) {
-    await browser.close();
-    browser = null;
-  }
+  // if (browser) {
+  //   await browser.close();
+  //   browser = null;
+  // }
 };
