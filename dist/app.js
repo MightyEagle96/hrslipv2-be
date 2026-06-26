0019ddcefa5779942ad7821de97f20a6728692d7
MIGHTY EAGLE <mightyeaglecorp@gmail.com>
2026-06-26 16:44:12 +0100

    made changes

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const pdf_routes_1 = __importDefault(require("./routes/pdf.routes"));
 const cors_1 = __importDefault(require("cors"));
+const node_path_1 = __importDefault(require("node:path"));
 const app = (0, express_1.default)();
 app
     .use((0, cors_1.default)())
     .use(express_1.default.json({ limit: "50mb" }))
+    .use(express_1.default.static(node_path_1.default.join(__dirname, "build")))
+    .use(express_1.default.static("public"))
     .use(express_1.default.urlencoded({ extended: true }))
     .use(express_1.default.static("public"))
-    .use("/api", pdf_routes_1.default)
+    .use("/api", pdf_routes_1.default);
+app
+    .use((req, res) => {
+    res.sendFile(node_path_1.default.join(__dirname, "build", "index.html"));
+})
     .listen(3000, () => console.log("Server running on port 3000"));
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import router from "./routes/pdf.routes";
 import cors from "cors";
+import path from "node:path";
 
 const app = express();
 
 app
   .use(cors())
   .use(express.json({ limit: "50mb" }))
+
+  .use(express.static(path.join(__dirname, "build")))
+  .use(express.static("public"))
+
   .use(express.urlencoded({ extended: true }))
   .use(express.static("public"))
-  .use("/api", router)
+  .use("/api", router);
+
+app
+  .use((req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  })
   .listen(3000, () => console.log("Server running on port 3000"));
